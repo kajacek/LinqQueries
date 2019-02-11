82a055bd921fc1b969c3a594820d79719399d49e
j.kasznia <j.kasznia@o2.pl>
2019-02-11 17:37:45 +0100

    Dodawanie

--- LinqQueries/MainForm.Designer.cs
+++ LinqQueries/MainForm.Designer.cs
@@ -30,6 +30,17 @@
         {
             this.getResult = new System.Windows.Forms.Button();
             this.dgv = new System.Windows.Forms.DataGridView();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbLastName = new System.Windows.Forms.TextBox();
+            this.dtpBirth = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.mtbHeight = new System.Windows.Forms.MaskedTextBox();
+            this.mtbWeight = new System.Windows.Forms.MaskedTextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,22 +61,129 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv.Location = new System.Drawing.Point(12, 12);
+            this.dgv.Location = new System.Drawing.Point(12, 91);
             this.dgv.Name = "dgv";
-            this.dgv.Size = new System.Drawing.Size(707, 428);
+            this.dgv.Size = new System.Drawing.Size(707, 349);
             this.dgv.TabIndex = 1;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(12, 42);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(100, 20);
+            this.tbName.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Last name";
+            // 
+            // tbLastName
+            // 
+            this.tbLastName.Location = new System.Drawing.Point(118, 42);
+            this.tbLastName.Name = "tbLastName";
+            this.tbLastName.Size = new System.Drawing.Size(100, 20);
+            this.tbLastName.TabIndex = 4;
+            // 
+            // dtpBirth
+            // 
+            this.dtpBirth.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpBirth.Location = new System.Drawing.Point(224, 42);
+            this.dtpBirth.Name = "dtpBirth";
+            this.dtpBirth.Size = new System.Drawing.Size(95, 20);
+            this.dtpBirth.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(221, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Birth date";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(325, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Height";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(390, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Weight";
+            // 
+            // mtbHeight
+            // 
+            this.mtbHeight.Location = new System.Drawing.Point(325, 42);
+            this.mtbHeight.Mask = "00000";
+            this.mtbHeight.Name = "mtbHeight";
+            this.mtbHeight.Size = new System.Drawing.Size(62, 20);
+            this.mtbHeight.TabIndex = 12;
+            this.mtbHeight.ValidatingType = typeof(int);
+            // 
+            // mtbWeight
+            // 
+            this.mtbWeight.Location = new System.Drawing.Point(393, 42);
+            this.mtbWeight.Mask = "00000";
+            this.mtbWeight.Name = "mtbWeight";
+            this.mtbWeight.Size = new System.Drawing.Size(62, 20);
+            this.mtbWeight.TabIndex = 13;
+            this.mtbWeight.ValidatingType = typeof(int);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(471, 40);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 14;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(754, 500);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.mtbWeight);
+            this.Controls.Add(this.mtbHeight);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dtpBirth);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbLastName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbName);
             this.Controls.Add(this.dgv);
             this.Controls.Add(this.getResult);
             this.Name = "MainForm";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -73,6 +191,17 @@
 
         private System.Windows.Forms.Button getResult;
         private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbLastName;
+        private System.Windows.Forms.DateTimePicker dtpBirth;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MaskedTextBox mtbHeight;
+        private System.Windows.Forms.MaskedTextBox mtbWeight;
+        private System.Windows.Forms.Button btnAdd;
     }
 }
 
--- LinqQueries/MainForm.cs
+++ LinqQueries/MainForm.cs
@@ -20,12 +20,34 @@ namespace LinqQueries
 
         private void getResult_Click(object sender, EventArgs e)
         {
-            ObjectRepo repo = new ObjectRepo();
-            repo.Add(new Person { Id = 1000, Name = "XYZ" });
             dgv.DataSource = null;
             dgv.DataSource = People.PeopleList
                 .OrderByDescending(a=>a.Id)
                 .ToList();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            ObjectRepo repo = new ObjectRepo();
+            //Person person = new Person();
+            //person.Name = tbName.Text;
+            //person.LastName = tbLastName.Text;
+            //person.BirthDate = dtpBirth.Value;
+            //person.Weight = Convert.ToInt32(mtbWeight.Text);
+            //person.Height = Convert.ToInt32(mtbHeight.Text);
+            Person person = new Person()
+            {
+                Name = tbName.Text,
+                LastName = tbLastName.Text,
+                BirthDate = dtpBirth.Value,
+                Weight = Convert.ToInt32(mtbWeight.Text),
+                Height = Convert.ToInt32(mtbHeight.Text),
+            };
+            repo.Add(person);
+            MessageBox.Show("OK");
+            getResult_Click(null, null);        // wywołanie metody z przycisku, bez obciążania pamięci
+            //getResult_Click(sender, e);        // wywołanie metody z przycisku
+
         }
     }
 }
